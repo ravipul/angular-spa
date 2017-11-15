796f7ad3ec266c5d8f82d2d0ca1a56bf97e0cb5b
Demis Bellot <demis.bellot@gmail.com>
2017-11-15 04:21:52 -0500

    Add MyApp client project

---
+++ MyApp/gulpfile.js
@@ -0,0 +1,27 @@
+(function () {
+    var SCRIPTS = {
+        'dev': 'npm run dev',
+        'dtos': 'npm run dtos',
+        'webpack-build': 'npm run build',
+        'webpack-build-prod': 'npm run build-prod',
+        'publish': 'npm run publish',
+        'tests-run': 'npm run test'
+    };
+
+    var gulp = require('gulp');
+    var exec = require('child_process').exec;
+
+    function runScript(script, done) {
+        process.env.FORCE_COLOR = 1;
+        var proc = exec(script + (script.startsWith("npm") ? " --silent" : ""));
+        proc.stdout.pipe(process.stdout);
+        proc.stderr.pipe(process.stderr);
+        proc.on('exit', () => done());
+    }
+
+    // Tasks
+    Object.keys(SCRIPTS).forEach(name => {
+        gulp.task(name, done => runScript(SCRIPTS[name], done));
+    });
+
+})();
